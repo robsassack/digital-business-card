d13cb5affb9f55be88c1c44b72ae54f14e67518e
Rob Sassack <rsassack25@gmail.com>
2022-10-26 14:07:47 -0400

    Added fonts, images, and component files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import './App.css'
+import './App.css';
+import Info from './components/Info'
+import About from './components/About';
+import Interests from './components/Interests';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <></>
+    <>
+      <Info />
+      <About />
+      <Interests />
+      <Footer />
+    </>
   )
 }
 
-export default App
+export default App;
