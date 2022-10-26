52fa799796570437cc1579d2bd46c64932a298a5
Rob Sassack <rsassack25@gmail.com>
2022-10-26 14:44:16 -0400

    About and Interests content added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 function About() {
   return (
-    <></>
+    <div className="about">
+      <h2 className="about--title">About</h2>
+      <p className="about--text">I am a web developer with a passion for building things. I like to solve problems and learn new things. I previously came from an IT background and I am ready for a new challenge.</p>
+    </div>
   );
 }
 
--- src/components/Interests.jsx
+++ src/components/Interests.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 function Interests() {
   return (
-    <></>
+    <div className="interests">
+      <h2 className="interests--title">Interests</h2>
+      <p className="interests--text">Programming, music, bass, guitar, reading, gaming, film.</p>
+    </div>
   );
 }
 
