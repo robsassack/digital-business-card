223cdb344a795ab4ea46501eddccbd6b3faab860
Rob Sassack <rsassack25@gmail.com>
2022-10-26 17:12:38 -0400

    Replaced .png icons with .svg icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import React from "react";
 function Footer() {
   return (
     <div className="footer">
-      <a href="https://github.com/robsassack"><img src="github.png"></img></a>
-      <a href="https://twitter.com/robsassasck"><img src="twitter.png"></img></a>
-      <a href="https://www.facebook.com/RobSassack/"><img src="facebook.png"></img></a>
-      <a href="https://www.instagram.com/robsassack/"><img src="instagram.png"></img></a>
+      <a href="https://github.com/robsassack"><img src="github.svg"></img></a>
+      <a href="https://twitter.com/robsassasck"><img src="twitter.svg"></img></a>
+      <a href="https://www.facebook.com/RobSassack/"><img src="facebook.svg"></img></a>
+      <a href="https://www.instagram.com/robsassack/"><img src="instagram.svg"></img></a>
     </div>
   );
 }
